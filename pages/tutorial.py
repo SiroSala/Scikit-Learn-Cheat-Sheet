7354c143c1ef25bf451c9f33fbe375196bd29eac
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2024-11-28 08:40:44 +0000

    tare datasets

--- pages/tutorial.py
+++ pages/tutorial.py
@@ -2,19 +2,18 @@ import streamlit as st
 import os
 
 def load_markdown_file(path):
+    """Utility function to read a Markdown file and return its contents."""
     with open(path, "r", encoding="utf-8") as file:
         return file.read()
 
-def main():
+def layout():
+    """Loads and displays the content of the Markdown file within the Streamlit app."""
     # Define the path to the Markdown file
     markdown_path = "content/tutorial.md"
     
-    # Load and display the content of the Markdown file
+    # Check if the file exists and display it; otherwise, show an error message
     if os.path.exists(markdown_path):
         tutorial_content = load_markdown_file(markdown_path)
         st.markdown(tutorial_content, unsafe_allow_html=True)
     else:
         st.error("Error: Tutorial content file not found.")
-
-if __name__ == "__main__":
-    main()
