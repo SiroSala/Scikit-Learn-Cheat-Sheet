16fd315497795dbda52c12169b26445caa6b57d6
Mejbah Ahammad <56669333+ahammadmejbah@users.noreply.github.com>
2024-11-28 10:22:36 +0000

    pagination

--- pages/tutorial.py
+++ pages/tutorial.py
@@ -11,19 +11,15 @@ def layout():
     # Define the base path to the Markdown files directory
     base_path = "content/90days"
     
+    # Initialize the session state for topic index if it's not already set
+    if 'selected_topic_index' not in st.session_state:
+        st.session_state['selected_topic_index'] = 0
+
     # Generate list of topics based on file naming convention
     topics = [f"Day{i}_Topic.md" for i in range(1, 91)]  # Adjust range as needed for your files
 
-    # Dropdown to select a topic
-    selected_topic_index = st.selectbox(
-        "Select a topic",
-        range(len(topics)),
-        index=0,  # Default to the first file
-        format_func=lambda x: topics[x].replace('_', ' ').replace('.md', '')  # Clean up file names for display
-    )
-
     # Define the path to the selected Markdown file
-    markdown_path = os.path.join(base_path, topics[selected_topic_index])
+    markdown_path = os.path.join(base_path, topics[st.session_state['selected_topic_index']])
     
     # Check if the file exists and display it; otherwise, show an error message
     if os.path.exists(markdown_path):
@@ -31,19 +27,17 @@ def layout():
         st.markdown(tutorial_content, unsafe_allow_html=True)
     else:
         st.error("Error: Tutorial content file not found.")
-    
-    # Pagination for previous and next topics
-    col1, col2 = st.columns([1, 1])
-    if selected_topic_index > 0:  # There is a previous topic
-        with col1:
-            if st.button("Previous"):
-                # Go to the previous topic
-                st.experimental_rerun()
-                st.session_state['selected_topic_index'] = selected_topic_index - 1
 
-    if selected_topic_index < len(topics) - 1:  # There is a next topic
-        with col2:
-            if st.button("Next"):
-                # Go to the next topic
+    # Create buttons for navigation between topics
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.session_state['selected_topic_index'] > 0:
+            if st.button('Previous'):
+                st.session_state['selected_topic_index'] -= 1
                 st.experimental_rerun()
-                st.session_state['selected_topic_index'] = selected_topic_index + 1
+
+    with col2:
+        if st.session_state['selected_topic_index'] < len(topics) - 1:
+            if st.button('Next'):
+                st.session_state['selected_topic_index'] += 1
+                st.experimental_rerun()
